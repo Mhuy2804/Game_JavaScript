da5e7ae07d1c17c21640e843c9e251e453caa496
Mhuy2804 <132818292+Mhuy2804@users.noreply.github.com>
2024-03-31 00:42:51 +0700

    Create_Particle

--- Gameplay_Dog_Shadow/main.js
+++ Gameplay_Dog_Shadow/main.js
@@ -2,6 +2,7 @@ import { Player } from "./player.js";
 import { InputHandler } from "./input.js";
 import { Background } from "./background.js";
 import { FlyingEnemy, GroundEnemy, ClimbingEnemy} from "./enemies.js";
+import { UI } from "./UI.js";
 
 window.addEventListener('load', function() {
     const canvas = document.getElementById('canvas1');
@@ -15,13 +16,20 @@ window.addEventListener('load', function() {
             this.height = height;
             this.groundMargin = 80;
             this.speed = 0;
-            this.maxSpeed = 4;
+            this.maxSpeed = 6;
             this.background = new Background(this);
             this.player = new Player(this);
-            this.input = new InputHandler();
+            this.input = new InputHandler(this);
+            this.UI = new UI(this);
             this.enemies = [];
+            this.particles = [];
             this.enemyTimer = 0;
             this.enemyInterval = 1000;
+            this.debug = true;
+            this.score = 0;
+            this.fontColor = 'black';
+            this.player.currentState = this.player.states[0];
+            this.player.currentState.enter();
         }
         update(deltaTime){
             this.background.update();
@@ -37,26 +45,36 @@ window.addEventListener('load', function() {
                 enemy.update(deltaTime);
                 if (enemy.markedForDeletion){
                     this.enemies.splice(this.enemies.indexOf(enemy), 1);
-                }
-            
+                }           
             });
+            // handle particles
+            this.particles.forEach((particle, index) => {
+                particle.update();
+                if (particle.markedForDeletion) this.particles.splice(index, 1);
+            });
+            console.log(this.particles);
         }
         draw(context) {
             this.background.draw(context);
             this.player.draw(context);
             this.enemies.forEach(enemy => {
-                enemy.draw(context);
-            
+                enemy.draw(context);            
             });
+            this.particles.forEach(particle => {
+                particle.draw(context);
+            });
+            this.UI.draw(context);
         }
         addEnemy(){
+            if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+            else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+            
+
             this.enemies.push(new FlyingEnemy(this));
-            console.log(this.enemies);
         }
     }
 
     const game = new Game(canvas.width, canvas.height);
-    console.log(game);
     let lastTime = 0;
 
     function animate(timpeStamp) {
